e722b5f9af39a60c57b3a2d66483f73c18286a45
muhmadjaradat <muhmadjradat@gmail.com>
2021-04-28 03:49:52 +0300

    adding lab03

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
+import React from 'react';
+import './App.css';
 import Main from './compentents/main';
 import Header from './compentents/header';
 import Footer from './compentents/footer';
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+import SelectedBeast from './compentents/SelectedBeast ';
+import Data from './data.json'
 
 class App extends React.Component {
+ 
+  constructor(props){
+    super(props);
+
+    this.state = {
+      data : Data,
+      isOpen : false,
+      selectedData: null
+    }
+  }
+    openModal = (e) => {
+      this.setState({ isOpen: true ,selectedData:e.target});
+      
+    }
+  closeModal = () => {this.setState({ isOpen: false });}
+ 
+    
   render() {
+    
     return (
+      <>
     <div>
+    
     <Header/>
-    <Main/>
+    <Main Data={this.state.data} openModal={this.openModal} />
     <Footer/>
+    {(this.state.isOpen )? <SelectedBeast selectedData={this.state.selectedData} isOpen={this.state.isOpen} closeModal={this.closeModal} />:null} 
+
     </div>
+      
+  
+     
+    </>
     )
   }
 }
